f94a2e330d0c44ef2aba04c6d950fd8ba1d52fba
trunghi1412 <91446424+trunghi1412@users.noreply.github.com>
2022-10-20 15:53:10 +0700

    Create index.js

---
+++ ex0.2/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root =
+      ReactDOM.createRoot(document.getElementById('root'));
+
+root.render (
+  <section>
+  <header>
+   <h1>A Header</h1>
+  </header>
+  <nav>
+   <a href="item"Nav Item</a>
+  </nav>
+  <main>
+   <p>The main content...</p>
+  </main>
+  <footer>
+   <small>&copy; 2021</small>
+  </footer>
+ </section>
+  };  
