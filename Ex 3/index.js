f86fd79c8f21962722de2297753e089eb5b70250
trunghi1412 <91446424+trunghi1412@users.noreply.github.com>
2022-10-20 17:10:04 +0700

    Create index.js

---
+++ Ex 3/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MyInput from './MyInput';
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MyInput />);
