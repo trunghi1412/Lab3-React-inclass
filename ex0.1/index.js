d65981043a5e536175f03129e4c1ca5fcd6144fe
trunghi1412 <91446424+trunghi1412@users.noreply.github.com>
2022-10-20 15:47:39 +0700

    Create index.js

---
+++ ex0.1/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = 
+      ReactDOM.createRoot(document.GetElemetById('roor'))
+
+root.render (
+  <div>
+   <button />
+   <code />
+   <input />
+   <label />
+   <p />
+   <pre />
+   <select />
+   <table />
+   <ul />
+  </div>
+ ); 
