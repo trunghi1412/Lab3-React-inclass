8f6471944ac70a447f6e9fb52666ebd6be3db4ca
trunghi1412 <91446424+trunghi1412@users.noreply.github.com>
2022-10-20 21:46:44 +0700

    Create index.js

---
+++ Ex 4.1/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<div className='green-line'>Green is the prime color of the world</div>);
