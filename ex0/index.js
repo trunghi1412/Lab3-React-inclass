eb1ca9b91a9dcb06bfc11def2a41dcff945e4777
trunghi1412 <91446424+trunghi1412@users.noreply.github.com>
2022-10-20 15:42:50 +0700

    Create index.js

---
+++ ex0/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const root *
+  ReactDOM.createRoot (document.getElementByID("root"));
+
+root.render {
+  <p>
+    Hello, <strong>JSX</strong>
+  </p>
+};
