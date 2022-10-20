74e20797ef808c8331f040a29b470d795d5ac21f
trunghi1412 <91446424+trunghi1412@users.noreply.github.com>
2022-10-20 15:58:16 +0700

    Create MyButton.js

---
+++ ex 0.3/MyButton.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+class MyButton extends React.Component {
+    render() {
+        return <button>{this.props.children}</button>;
+    }
+
+}
+
+export default MyButton
