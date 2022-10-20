7a3f154e2151c1d18318a9e06c7955c731dba813
trunghi1412 <91446424+trunghi1412@users.noreply.github.com>
2022-10-20 17:11:07 +0700

    Add files via upload

---
+++ Ex 3/MyInput.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+class MyInput extends React.Component {
+    onChange() {
+        console.log("changed");
+    }
+
+    onBlur() {
+        console.log("blured");
+    }
+
+    render() {
+        return <input onChange={this.onChange} onBlur={this.onBlur} />;
+
+    }
+}
+
+export default MyInput
